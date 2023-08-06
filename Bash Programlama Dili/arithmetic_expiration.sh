b47772c1a279d74cae48ef60b6b508a270b1e9e2
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2023-08-06 16:58:52 +0300

    2- Aritmetik İfadeler

--- Bash Programlama Dili/arithmetic_expiration.sh
+++ Bash Programlama Dili/arithmetic_expiration.sh
@@ -1,42 +1,3 @@
-# 1- Kullanıcıdan Veri Alma
-```bash
-#!/bin/bash
-
-# yorum satırı
-
-echo Program Adı:  $0 # program adını verir
-
-echo Degisken1: $1 Degisken2: $2
-
-echo Process Id: $$ # Bu scriptin çalışmış olduğu process id'yi yani işlem idsini verecek
-
-echo User: $USER # Bu programın çalıştığı kullanıcı
-
-echo Random Number : $RANDOM # Rastgele numara üretir.
-
-echo Hostname: $HOSTNAME # Hostname'i bildirir.
-
-echo "Merhaba Dünya!!!"
-
-echo "****************************************************************************"
-
-myVariable="Merhaba Dünya"
-
-echo $myVariable
-
-read varname 
-
-echo "varname: "$varname
-
-read -p "Username: " username
-read -sp "Password: " password
-
-echo Username: $username
-echo Password: $password
-```
-
-# 2- Aritmetik İfadeler
-```bash
 # let <arithmetic expression>
 echo "********************** let **********************"
 let a=5+4
@@ -112,4 +73,4 @@ echo ${#a}
 
 b=1234
 echo ${#b}
-```
+
